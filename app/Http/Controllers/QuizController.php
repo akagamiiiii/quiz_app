b990c30fac04ba4fad4995cba0b362a6292a49f6
akagami <neko88803@gmail.com>
2025-10-28 23:36:11 +0900

    クイズ新規登録画面実装

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreQuizRequest;
 use App\Http\Requests\UpdateQuizRequest;
-use App\Models\Quiz;
 
 class QuizController extends Controller
 {
@@ -17,11 +18,13 @@ class QuizController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * クイズ新規登録画面表示
      */
-    public function create()
+    public function create(Request $request, int $categoryId)
     {
-        //
+        return view('admin.quizzez.create', [
+            'categoryId' => $categoryId
+        ]);
     }
 
     /**
