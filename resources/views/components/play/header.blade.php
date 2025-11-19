5e093d36f0fa5de57865a9d2c338b5d551db13fa
akagami <neko88803@gmail.com>
2025-11-19 23:10:38 +0900

    プレイヤー画面レイアウト作成

---
+++ resources/views/components/play/header.blade.php
@@ -0,0 +1,31 @@
+<header class="text-gray-600 body-font bg-blue-200">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a href="" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">MyQuizApp</span>
+    </a>
+  </div>
+</header>
+
+{{-- <header class="header">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <div class="header-container">
+    <a class="header-logo">
+      <svg class="header-icon">...</svg>
+      <span class="header-title">管理画面</span>
+    </a>
+
+    <form method="POST" action="{{ route('logout') }}" class="header-logout">
+        @csrf
+        <button
+          :href="route('logout')"
+                onclick="event.preventDefault();
+                            this.closest('form').submit();"
+        >ログアウト
+          <svg class="header-logout-icon"></svg>
+        </button>
+    </form>
+  </div>
+</header> --}}
